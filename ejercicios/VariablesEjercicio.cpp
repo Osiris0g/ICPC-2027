b17c372f35d1fe70c5d9902846f685a68ea0920a
Osiris Karim Aguirre Coli <126359730+Osiris0g@users.noreply.github.com>
2026-09-02 20:27:24 -0600

    Update and rename VariablesEjercicio to VariablesEjercicio.cpp

---
+++ ejercicios/VariablesEjercicio.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int miEntero;
+    int miEntero2;
+    int miEntero3;
+    
+    cin >> miEntero;
+    cin >> miEntero2;
+    cin >> miEntero3;
+
+    cout << miEntero3 << endl;
+    cout << miEntero2 << endl;
+    cout << miEntero  << endl;
+    return 0;
+}
